8f7c5b4dc610ca3dba06f092a130b7a2ff6ebac4
rudnam <francisalbarracin2@gmail.com>
2022-08-30 16:28:33 +0800

    Add rainbow and grid toggles + improve canvas size selector

--- script.js
+++ script.js
@@ -3,39 +3,54 @@ var isMouseDown = false;
 const canvas = document.querySelector('#canvas');
 const reset = document.querySelector('#reset');
 const resize = document.querySelector('#resize');
+const rainbow = document.querySelector('#rainbow');
+const grid = document.querySelector('#grid');
 
 document.body.onmousedown = function(e) {
     if (canvas.contains(e.target)) {
         e.preventDefault();
     }
     isMouseDown = true;
-    console.log('down')
-}
+};
+
 document.body.onmouseup = function() {
     isMouseDown = false;
-    console.log('up');
-}
+};
 
 for (let i = 0; i < SIZE; i++) {
     var column = document.createElement("div");
     column.classList.add("column");
-    canvas.appendChild(column)
+    canvas.appendChild(column);
     for (let j = 0; j < SIZE; j++) {
         var cell = document.createElement("div");
         cell.classList.add("cell");
-        cell.addEventListener("click", paintBlack);
-        cell.addEventListener("mouseenter", paintBlack2);
+        if (grid.checked) {
+            cell.style.border = '1px solid red';
+        }
+        cell.addEventListener("click", paintCanvas);
+        cell.addEventListener("mouseenter", paintCanvas2);
         column.appendChild(cell);
     }
 }
 
-function paintBlack(e) {
-    e.target.style.backgroundColor="black";
+function paintCanvas(e) {
+    if (rainbow.checked) {
+        e.target.style.backgroundColor = "orange";
+    }
+    else {
+        e.target.style.backgroundColor = "black";
+    }
 }
 
-function paintBlack2(e) {
+function paintCanvas2(e) {
     if (isMouseDown) {
-        e.target.style.backgroundColor="black"
+        if (rainbow.checked) {
+            var randomColor = Math.floor(Math.random()*16777215).toString(16);
+            e.target.style.backgroundColor = '#' + randomColor;
+        }
+        else {
+            e.target.style.backgroundColor = "black";
+        }
     }
 }
 
@@ -47,8 +62,8 @@ function resetCanvas() {
 }
 
 function resizeCanvas() {
-    var newSize = prompt('Enter new size (max 100):');
-    if (newSize < 101) {
+    var newSize = resize.value;
+    if (newSize < 65) {
         SIZE = newSize;
         while(canvas.hasChildNodes()) {
             canvas.removeChild(canvas.lastChild);
@@ -56,17 +71,35 @@ function resizeCanvas() {
         for (let i = 0; i < SIZE; i++) {
             var column = document.createElement("div");
             column.classList.add("column");
-            canvas.appendChild(column)
+            canvas.appendChild(column);
             for (let j = 0; j < SIZE; j++) {
                 var cell = document.createElement("div");
                 cell.classList.add("cell");
-                cell.addEventListener("click", paintBlack);
-                cell.addEventListener("mouseenter", paintBlack2);
+                if (grid.checked) {
+                    cell.style.border = '1px solid red';
+                }
+                cell.addEventListener("click", paintCanvas);
+                cell.addEventListener("mouseenter", paintCanvas2);
                 column.appendChild(cell);
             }
         }
     } 
 }
 
+function toggleGrid() {
+    let cells = document.getElementsByClassName('cell');
+    if (grid.checked) {
+        for(let index = 0; index < cells.length ; ++index) {
+            cells[index].style.border = '1px solid red';
+        }
+    }
+    else {
+        for(let index = 0; index < cells.length ; ++index) {
+            cells[index].style.border = '';
+        }
+    }
+}
+
 reset.addEventListener('click', resetCanvas);
-resize.addEventListener('click', resizeCanvas);
+resize.addEventListener('change', resizeCanvas);
+grid.addEventListener('change', toggleGrid);
