3d0b4c4fed4c16090854520d02a84c3aadc12c37
rudnam <francisalbarracin2@gmail.com>
2022-08-30 11:39:09 +0800

    Implement etch-a-sketch

---
+++ script.js
@@ -0,0 +1,53 @@
+var SIZE = 16;
+const canvas = document.querySelector('#canvas');
+const reset = document.querySelector('#reset');
+const resize = document.querySelector('#resize');
+
+for (let i = 0; i < SIZE; i++) {
+    var column = document.createElement("div");
+    column.classList.add("column");
+    canvas.appendChild(column)
+    for (let j = 0; j < SIZE; j++) {
+        var cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.addEventListener("mouseenter", paintBlack);
+        column.appendChild(cell);
+    }
+}
+
+
+function paintBlack(e) {
+    e.target.style.backgroundColor="black";
+}
+
+function resetCanvas() {
+    let cells = document.getElementsByClassName('cell');
+    for(let index = 0; index < cells.length ; ++index) {
+        cells[index].style.backgroundColor = 'white';
+    }
+}
+
+function resizeCanvas() {
+    var newSize = prompt('Enter new size (max 100):');
+    if (newSize < 101) {
+        SIZE = newSize;
+        while(canvas.hasChildNodes()) {
+            canvas.removeChild(canvas.lastChild);
+        }
+        for (let i = 0; i < SIZE; i++) {
+            var column = document.createElement("div");
+            column.classList.add("column");
+            canvas.appendChild(column)
+            for (let j = 0; j < SIZE; j++) {
+                var cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.addEventListener("mouseenter", paintBlack);
+                column.appendChild(cell);
+            }
+        }
+    }
+    
+}
+
+reset.addEventListener('click', resetCanvas);
+resize.addEventListener('click', resizeCanvas);
